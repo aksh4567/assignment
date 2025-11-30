e2132e462b41d48ad201328996f28cd469937a59
Akshdeep Singh <akshsingh977@gmail.com>
2025-11-30 15:35:24 +0530

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,42 @@
-import Image from "next/image";
+import { Base } from "@/components/Base";
+
 
 export default function Home() {
+  const componentData = {
+    name: "Super",
+    count: 30,
+    images: [
+      {
+        url: "https://imgs.search.brave.com/hay-HxCMOmpUNxJlVO109_kWOMjyUDYSKJuiZFzxve0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/bWFnaWNkZWNvci5p/bi9jb20vMjAyMy8w/Mi8yOTE3NTkwNy9p/bWFnZS0xNjkwMzY4/Mzg4LTU3OS0zMjB4/MzIwLmpwZw",
+        ready: true,
+        error: false
+      },
+      {
+        url: "https://imgs.search.brave.com/O6CaUb_HsL1gObywwTp9xKI2rjYwSpGrJ51_ijLwPeY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA1LzQxLzM5LzIw/LzM2MF9GXzU0MTM5/MjAxNl9FODhRcDRv/aEh3VHVOZE5IcFNJ/Qkk3aGVZTzJMNEdi/Mi5qcGc",
+        ready: true,
+        error: false
+      },
+      {
+        url: "",
+        ready: false,
+        error: true
+      },
+      {
+        url: "",
+        ready: false,
+        error: false
+      }
+    ]
+  };
+
   return (
- 
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Base data={componentData} />
+    </div>
   );
 }
+
+
+
+
+
--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,3 +1,7 @@
 export const Base = () => {
-  return <div className="bg-black h-60 w-300"></div>;
+  return (
+    <div className="bg-black h-60 w-300 p-8">
+      <div className=""></div>
+    </div>
+  );
 };
